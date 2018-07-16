d0a3947b06ee3921110aa661a4b56a4e3fbe1864
Liam Dyer <saghendev@gmail.com>
2018-07-16 13:16:43 -0400

    Working on News Admin Panel and Small changes

--- index.js
+++ index.js
@@ -1,47 +1,39 @@
 let express = require('express'),
     app = express(),
     path = require('path'),
-    compression = require('compression');
+    compression = require('compression'),
+    formidable = require('express-formidable'),
+    fs = require('fs');
 
 app.use(compression());
+app.use(formidable());
 app.use(express.static('static'));
 
 app.get('/update-news', (req, res) => {
     if(req.query.token != 'gHsGEw7u4YTcaOf') {
-        res.status(401).send('Token was incorrect. Please email liamcdyer@gmail.com if a new token is needed');
+        res.status(401).send('Token was incorrect. Please email liamcdyer@gmail.com if a new token is needed.');
         return;
     }
-    res.sendFile(path.resolve(__dirname, 'update_news/index.html'));
+    res.sendFile(path.resolve(__dirname, 'update-news/index.html'));
+});
+
+app.post('/update-news', (req, res) => {
+    if(req.query.token != 'gHsGEw7u4YTcaOf') {
+        res.status(401).send('Token was incorrect. Please email liamcdyer@gmail.com if a new token is needed.');
+        return;
+    }
+    fs.writeFile(path.resolve(__dirname, 'static/js/news.json'), JSON.stringify(req.body));
+    res.json({success: true});
 })
 
 require('greenlock-express').create({
-
-    // Let's Encrypt v2 is ACME draft 11
     version: 'draft-11'
-  
-    // Note: If at first you don't succeed, switch to staging to debug
-    // https://acme-staging-v02.api.letsencrypt.org/directory
   , server: 'https://acme-v02.api.letsencrypt.org/directory'
-  
-    // Where the certs will be saved, MUST have write access
   , configDir: '~/.config/acme/'
-  
-    // You MUST change this to a valid email address
   , email: 'saghendev@gmail.com'
-  
-    // You MUST change these to valid domains
-    // NOTE: all domains will validated and listed on the certificate
   , approveDomains: [ 'virtuscapital.ca' ]
-  
-    // You MUST NOT build clients that accept the ToS without asking the user
   , agreeTos: true
-  
   , app: app
-  
-    // Join the community to get notified of important updates
   , communityMember: false
-  
-    // Contribute telemetry data to the project
   , telemetry: false
-  
   }).listen(80, 443);
